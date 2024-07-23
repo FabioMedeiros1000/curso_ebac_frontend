6a314b6ee33561f6c1e99d275ec729e7058d1b66
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2024-07-23 17:05:07 -0300

    Commit inicial

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-$(document).ready(function(){
-    $('button').on('click',function(e){
-        e.preventDefault();
-        const novaTarefa = $('input').val();
-        const listaDeTarefas = $(`<li style='display:none'>${novaTarefa}</li>`);
-        $(listaDeTarefas).appendTo('ol');
-        $(listaDeTarefas).fadeIn(500);
-        $('input').val(''); 
+document.addEventListener('DOMContentLoaded', function(){
+    const endpoint = "https://api.github.com/users/FabioMedeiros1000";
+
+    // fetch(endpoint).then(function(resposta){
+    //     resposta.json();
+    // }).then(function(json){
+    //     console.log(json.login);
+    // })
+
+    fetch(endpoint).then(function(resposta){
+        return resposta.json();
     })
+    .then(function(json){
+        const usuario = `@${json.login}`;
+        const seguidores = json.followers;
+        const seguindo = json.following;
+        const repositoriosPrivados = 1;
+        const repositorios = json.public_repos + repositoriosPrivados;
+        const linkGitHub = json.html_url;
 
-    $('ol').on('click', 'li', function(e) {
-        $(this).toggleClass('strikethrough');
-    });
+        const profileUsername = document.querySelector('.profile-username');
+        const repositories = document.querySelector('#repositorios');
+        const followers = document.querySelector('#seguidores');
+        const following = document.querySelector('#seguindo');
+        const profileAvatar = document.querySelector('.profile-avatar');
+        const profileLink = document.querySelector('.profile-link');
 
-});
+        profileUsername.innerHTML += usuario;
+        repositories.innerHTML += repositorios;
+        followers.innerHTML += seguidores;
+        following.innerHTML += seguindo;
+        profileAvatar.src = json.avatar_url;
+        profileLink.href = linkGitHub;
+        // console.log(json);
+    })
+    .catch(function(erro){
+        alert("Erro inesperado! Por favor, aguarde para que providências sejam tomadas!")
+    })
+})
