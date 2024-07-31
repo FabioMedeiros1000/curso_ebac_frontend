f226791d1cdc4fd5b688bac56752eda0f227f14e
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2024-07-31 10:37:51 -0300

    Commit inicial

--- src/main.js
+++ src/main.js
@@ -1,21 +1,5 @@
-class Aluno{
-    constructor(nome, nota){
-        this.nome = nome;
-        this.nota = nota;
-    }
-}
-
-joao = new Aluno('João', 7.5)
-paulo = new Aluno('Paulo', 5.0)
-rodrigo = new Aluno('Rodrigo', 10.0)
-marcia = new Aluno('Márcia', 4.5)
-lucas = new Aluno('Lucas', 6.0)
-jeferson = new Aluno('Rodrigo', 3.5)
-
-alunos = [joao, paulo, rodrigo, marcia, lucas, jeferson]
-
-alunosAprovados = alunos.filter(function(objeto){
-    return objeto.nota >= 6;
-})
-
-console.log(alunosAprovados)
+"use strict";
+const multiplica = (n1, n2) => n1 * n2;
+const saudacao = (nome) => "Olá " + nome;
+console.log(multiplica(2, 3));
+console.log(saudacao('Pedro'));
